d0dc1cc055dcdc4760add5dc4169d0cd0c0dafac
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2024-03-04 17:46:45 -0600

    add support for paired version of doTTest function

--- UW.Adult.Covid.19/R/ttest.R
+++ UW.Adult.Covid.19/R/ttest.R
@@ -12,7 +12,11 @@
 #' @return data.frame of results
 #' @export
 #'
-doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.equal = TRUE, alternative="two.sided",debug=FALSE) {
+doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.equal = TRUE, 
+                  alternative="two.sided",debug=FALSE, is_paired=FALSE) {
+  if (is_paired) {
+    message("Running a paired t-test as requested. If unpaired test desired set is_paired = FALSE (default).")
+  }
 
   if (!("ID" %in% colnames(data_in))) {
     if (label.col == "row.names") {
@@ -29,16 +33,17 @@ doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.e
 
   group2=data.frame(data_in[,col2]);
   group1=data.frame(data_in[,col1]);
-  mean12        = rep(NA, nrow(data_in));
+  base_mean        = rep(NA, nrow(data_in));
   mean1         = rep(NA, nrow(data_in));
   mean2         = rep(NA, nrow(data_in));
   fold_changes  = rep(NA, nrow(data_in));
+  stderr1       = rep(NA, nrow(data_in));
+  stderr2       = rep(NA, nrow(data_in));
   pvalues       = rep(NA, nrow(data_in));
   npvalues      = rep(NA, nrow(data_in));
   ttest_values  = rep(NA, nrow(data_in));
-  stderr12      = rep(NA, nrow(data_in));
-  stderr1       = rep(NA, nrow(data_in));
-  stderr2       = rep(NA, nrow(data_in));
+  base_stderr      = rep(NA, nrow(data_in));
+
   df = rep(NA, nrow(data_in))
   if (debug) {cat("group1:",dim(group1),"\n");}
   if (debug) {cat("group2:",dim(group2),"\n");}
@@ -55,7 +60,7 @@ doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.e
     if(debug){cat("n1:",n1," n2:",n2,"\n");}
     if(debug){cat(dim(group1_gene),"\n");}
     if(debug){cat(dim(group2_gene),"\n");}
-    current_mean12 = NA;
+    current_base_mean = NA;
     current_mean1 = NA;
     current_mean2 = NA;
 
@@ -65,7 +70,7 @@ doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.e
 
     if (n1 > 0 || n2 > 0)
     {
-      current_mean12 = mean(c(group1_gene, group2_gene));
+      current_base_mean = mean(c(group1_gene, group2_gene));
     }
     if (n1 > 0) {
       current_mean1 = mean(group1_gene);
@@ -77,29 +82,39 @@ doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.e
       #print(group1_gene);
       #print(group2_gene);
 
-      temp=try(stats::t.test(x=group1_gene, y=group2_gene, var.equal=var.equal, alternative=alternative));
+      temp=try(stats::t.test(x=group1_gene, y=group2_gene, var.equal=var.equal, 
+                             alternative=alternative, paired = is_paired));
       if ("try-error" %in% class(temp)) {
         current_foldchange = 0;
         current_pvalue = 1;
         current_ttest_value = 0;
       } else {
-        current_mean1 = temp$estimate[1];
-        current_mean2 = temp$estimate[2];
+        # make compatible with paired test
+        current_mean1 = mean(group1_gene);
+        current_mean2 = mean(group2_gene);
         current_ttest_value = -temp$statistic;
-        current_foldchange = as.numeric(temp$estimate[2]-temp$estimate[1]);
+        if (is_paired) {
+          current_foldchange <- temp$estimate[1] # mean of the differences
+        } else {
+          current_foldchange = as.numeric(temp$estimate[2]-temp$estimate[1]);
+        }
         current_pvalue = as.numeric(temp$p.value);
         if (current_pvalue == "NaN") {
             current_pvalue = 1.0;
         }
         stderr1[idx] = sd(group1_gene);
         stderr2[idx] = sd(group2_gene);
-        stderr12[idx] = sd(c(group1_gene, group2_gene))
+        base_stderr[idx] = sd(c(group1_gene, group2_gene))
         df[idx] = temp$parameter;
       }
     } else if (n1 >= 1 && n2 >= 1) {
-      current_foldchange= (current_mean2 - current_mean1);
+      if (is_paired) {
+        current_foldchange <- temp$estimate[1] # mean of the differences
+      } else {
+        current_foldchange= (current_mean2 - current_mean1);
+      }
     }
-    mean12[idx] = current_mean12;
+    base_mean[idx] = current_base_mean;
     mean1[idx] = current_mean1;
     mean2[idx] = current_mean2;
     ttest_values[idx] = current_ttest_value;
@@ -122,16 +137,36 @@ doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.e
 
   res<-mt.rawp2adjp(npvalues,c("BH"),na.rm=TRUE);
   adjnp=res$adjp[order(res$index),];
-
-  ans=data.frame(
+  
+  if (is_paired) {
+    ans=data.frame(
     ID=data_in$ID,
-    mean12 = mean12,
+    base_mean = base_mean,
     mean1 = mean1,
     mean2 = mean2,
     statistic = ttest_values,
     stderr1 = stderr1,
     stderr2 = stderr2,
-    stderr12 = stderr12,
+    base_stderr = base_stderr,
+    df = df,
+    mean_of_diffs=2^fold_changes,
+    log2_mean_of_diffs = fold_changes,
+    pvalue=pvalues,
+    npvalue = npvalues,
+    padj=adjp[,2],
+    npadj=adjnp[,2],
+    stringsAsFactors=FALSE)
+    ans$foldChange[is.na(ans$mean_of_diffs)] = 1.0;
+  } else {
+    ans=data.frame(
+    ID=data_in$ID,
+    base_mean = base_mean,
+    mean1 = mean1,
+    mean2 = mean2,
+    statistic = ttest_values,
+    stderr1 = stderr1,
+    stderr2 = stderr2,
+    base_stderr = base_stderr,
     df = df,
     foldChange=2^fold_changes,
     log2FC = fold_changes,
@@ -139,11 +174,11 @@ doTTest<-function(data_in, col1, col2, col3 = NA, label.col = "row.names", var.e
     npvalue = npvalues,
     padj=adjp[,2],
     npadj=adjnp[,2],
-    stringsAsFactors=FALSE
-    );
+    stringsAsFactors=FALSE)
+    ans$foldChange[is.na(ans$foldChange)] = 1.0;
+  }
   rownames(ans) = ans$ID;
   #ans = ans[!is.na(ans$foldChange),];
-  ans$foldChange[is.na(ans$foldChange)] = 1.0;
   ans$pvalue[is.na(ans$pvalue)] = 1.0;
   ans$padj[is.na(ans$padj)] = 1.0;
   ans = ans[order(ans$padj,ans$pvalue),];
